eeaa4f9d646412bf674d4e1c658a65751afc4a00
Greg <garnold.805@gmail.com>
2022-04-29 10:25:01 -0400

    Completed Assignment

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  console.log(onChangeColor);
+
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  return (
+    // when this div is clicked, it will use onChangeColor variable to determine what function to run
+    // onChangeColor is a prop that is passed down from the Parent component, which references the handleChangeColor function
+    // The handleChangeColor function is the function that will actually run when the div is clicked, and will update state
+    //    in the Parent component
+    <div
+      onClick={handleClick}
+      className="child"
+      // pass in the color props data into Child component
+      style={{ backgroundColor: color }}
+    />
+  );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,21 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(randomColor);   // initial value for color state
+  const [childrenColor, setChildrenColor] = useState("#FFF");  // variable to keep track of the color of the Child componenets using state
+
+  // newChildColor taken in to send data back to the parent
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);  // update color state to a new value
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      {/* passing the data that represents color of Child components down to Parent */}
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
